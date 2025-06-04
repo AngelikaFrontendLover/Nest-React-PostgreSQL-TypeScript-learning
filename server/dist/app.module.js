12d1805ad7706f11da1bbffa6b7e2e65e0ac6d66
angelika <angelika.kozlova09@gmail.com>
2025-06-04 23:54:04 +0300

    add user, auth, category, transaction entities

--- server/dist/app.module.js
+++ server/dist/app.module.js
@@ -10,12 +10,16 @@ exports.AppModule = void 0;
 const common_1 = require("@nestjs/common");
 const app_controller_1 = require("./app.controller");
 const app_service_1 = require("./app.service");
+const user_module_1 = require("./user/user.module");
+const category_module_1 = require("./category/category.module");
+const auth_module_1 = require("./auth/auth.module");
+const transaction_module_1 = require("./transaction/transaction.module");
 let AppModule = class AppModule {
 };
 exports.AppModule = AppModule;
 exports.AppModule = AppModule = __decorate([
     (0, common_1.Module)({
-        imports: [],
+        imports: [user_module_1.UserModule, category_module_1.CategoryModule, auth_module_1.AuthModule, transaction_module_1.TransactionModule],
         controllers: [app_controller_1.AppController],
         providers: [app_service_1.AppService],
     })
