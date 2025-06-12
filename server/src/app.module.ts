96f9754d726997f3c49555e56523b68a3b7a7134
angelika <angelika.kozlova09@gmail.com>
2025-06-12 16:30:05 +0300

    add basic configurations

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,10 +5,25 @@ import { UserModule } from './user/user.module';
 import { CategoryModule } from './category/category.module';
 import { AuthModule } from './auth/auth.module';
 import { TransactionModule } from './transaction/transaction.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule, CategoryModule, AuthModule, TransactionModule],
+  imports: [UserModule, CategoryModule, AuthModule, TransactionModule, ConfigModule.forRoot(), TypeOrmModule.forRootAsync({
+    imports: [ConfigModule],
+    useFactory: (configService: ConfigService) => ({
+      type: 'postgres',
+      host: configService.get('DB_HOST'),
+      port: configService.get('DB_PORT'),
+      username: configService.get('DB_username'),
+      password: configService.get('DB_password'),
+      database: configService.get('DB_name'),
+      synchronize: true,
+      entities: [__dirname + '/**/*.entity{.js,.ts}']
+    }),
+    inject: [ConfigService]
+  })],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule { }
