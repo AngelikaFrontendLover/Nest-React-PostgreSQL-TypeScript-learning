3cd528b872058c2bdfb2b447861d8f4003758e92
angelika <angelika.kozlova09@gmail.com>
2025-09-22 18:56:29 +0300

    functions for transactions

--- server/src/transaction/entities/transaction.entity.ts
+++ server/src/transaction/entities/transaction.entity.ts
@@ -1,11 +1,11 @@
 import { Category } from "src/category/entities/category.entity";
 import { User } from "src/user/entities/user.entity";
-import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, OneToMany, PrimaryColumn, UpdateDateColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, OneToMany, PrimaryColumn, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
 @Entity()
 
 export class Transaction {
-    @PrimaryColumn({ name: 'transaction_ID' })
+    @PrimaryGeneratedColumn({ name: 'transaction_ID' })
     id: number;
 
     @Column()
@@ -14,9 +14,9 @@ export class Transaction {
     @Column({ nullable: true })
     type: string
 
-    user: User;
 
     @ManyToOne(() => User, (user) => user.transactions)
+    user: User;
     @JoinColumn({ name: 'user_id' })
 
     @ManyToOne(() => Category, (category) => category.transactions)
