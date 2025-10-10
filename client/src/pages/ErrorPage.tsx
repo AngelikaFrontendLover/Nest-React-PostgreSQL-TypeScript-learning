8609a5c6013a59aaaec6562ab0d801fcb8d9939e
angelika <angelika.kozlova09@gmail.com>
2025-10-10 17:28:17 +0300

    add header, error page

--- client/src/pages/ErrorPage.tsx
+++ client/src/pages/ErrorPage.tsx
@@ -1,7 +1,12 @@
 import { type FC } from "react";
+import img from '../assets/404.png'
+import { Link } from "react-router-dom";
 
 const ErrorPage: FC = () => {
-    return <div>ErrorPage</div>
+    return <div className="min-h-screen bg-slate-900 font-roboto text-white flex justify-center items-center flex-col gap-10">
+        <img src={img} className="w-80" alt="error-page" />
+        <Link to={'/'} className="bg-sky-500 rounded-md px-6">Back</Link>
+    </div>
 }
 
 export default ErrorPage;
